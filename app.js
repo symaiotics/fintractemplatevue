521d334b704f98c207bacaabffd71bde5eb4fec1
Amy-Zhou <zxyamyamy@gmail.com>
2021-06-24 16:25:34 -0400

    added country address

--- app.js
+++ app.js
@@ -27,10 +27,12 @@ axios({
             var name = filteredList[i].FIRST_NAME + " " + filteredList[i].SECOND_NAME + " " + filteredList[i].THIRD_NAME;
             var document = filteredList[i].INDIVIDUAL_DOCUMENT.TYPE_OF_DOCUMENT + " :" + filteredList[i].INDIVIDUAL_DOCUMENT.NUMBER;
             var dob = filteredList[i].INDIVIDUAL_DATE_OF_BIRTH.DATE;
+            var address = filteredList[i].INDIVIDUAL_ADDRESS.COUNTRY;
             finalList[i] = {
                 "name": name, 
                 "documentType":document, 
                 "dateOfBirth": dob,
+                "countryAddress":address,
                 "source": "https://scsanctions.un.org/resources/xml/en/consolidated.xml",
             };
         }
