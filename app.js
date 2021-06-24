3ae63bba5a79aa0b7e62c18f20aca37bc653e211
Milad Rezaee <miladrezaee@MacBook-Air.local>
2021-06-24 14:08:27 -0400

    Added .gitignore file and modified app.js to use xml-js library instead

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
 var axios = require('axios');
-var xmlParser = require('xml2json');
+var xmlParser = require('xml-js');
 
 var data = "";
 
 var masterListJSON = {
-    jSON_list: []
+    JSON_list: []
 };
 
 axios({
@@ -14,31 +14,33 @@ axios({
     url: 'https://www.international.gc.ca/world-monde/assets/office_docs/international_relations-relations_internationales/sanctions/sema-lmes.xml'
     })
     .then(function (response) {
-        data = JSON.parse(xmlParser.toJson(response.data))
+        data = xmlParser.xml2js(response.data, {compact: true});
     });
 
 app.get("/", (req, res) => {
 
     for (var i = 0; i < data["data-set"].record.length; i++) {
 
+        var entity = {};
+
         if (data["data-set"].record[i].Entity == null) {
-            masterListJSON.jSON_list.push({
-                name: data["data-set"].record[i].GivenName + " " + data["data-set"].record[i].LastName,
-                date: data["data-set"].record[i].DateOfBirth,
-                source: "https://www.international.gc.ca/world-monde/international_relations-relations_internationales/sanctions/consolidated-consolide.aspx?lang=eng"
-            })
+            // Given Name
+            entity["name"] = data["data-set"].record[i].GivenName ? data["data-set"].record[i].GivenName._text : null;
+            // Last Name
+            entity["name"] = entity.name + " " + (data["data-set"].record[i].LastName ? data["data-set"].record[i].LastName._text : null);
         } else {
-            masterListJSON.jSON_list.push({
-                name: data["data-set"].record[i].Entity,
-                date: data["data-set"].record[i].DateOfBirth,
-                source: "https://www.international.gc.ca/world-monde/international_relations-relations_internationales/sanctions/consolidated-consolide.aspx?lang=eng"
-            })
+            // Entity Name
+            entity["name"] = data["data-set"].record[i].Entity._text;
         }
+
+        entity["date"] = data["data-set"].record[i].DateOfBirth ? data["data-set"].record[i].DateOfBirth._text : null;
+        entity["source"] = "https://www.international.gc.ca/world-monde/international_relations-relations_internationales/sanctions/consolidated-consolide.aspx?lang=eng";
+
+        masterListJSON.JSON_list.push(entity);
     }
 
     if (!res.headersSent) res.status(200).send({ 
         masterListJSON
-        // data
     }) 
 })
 
