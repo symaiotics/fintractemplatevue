f039678c5ab5f944586d54fc2f9eb11a4d5da056
Milad Rezaee <miladrezaee@MacBook-Air.local>
2021-07-09 12:58:06 -0400

    Put api request in separate file

--- app.js
+++ app.js
@@ -24,50 +24,9 @@ var newPersonEntity;
 
 var consolidatedList = [];
 
-var masterListJSON = {
-	jsonList: [],
-};
+var ccas_list = require("./api/ccas").masterListJSON;
 
-const insertDataIntoMasterList = (data) => {
-	for (elem of data["data-set"].record) {
-		var entity = {
-			name: elem.Entity ? elem.Entity : elem.GivenName + " " + elem.LastName,
-			date: elem.DateOfBirth,
-			link: "https://www.international.gc.ca/world-monde/international_relations-relations_internationales/sanctions/consolidated-consolide.aspx?lang=eng",
-			address: {
-				streetNum: faker.datatype.number(),
-				street: faker.address.streetName(),
-				city: faker.address.cityName(),
-				prov: faker.address.state(),
-				postal: faker.address.zipCodeByState(),
-			},
-			id: {
-				idType: "Driver's License",
-				idNumber: faker.finance.routingNumber(),
-				idJuristiction: faker.address.state(),
-			},
-			accountInfo: {
-				locale: "Domestic",
-				institution: "TD",
-				transitNum: faker.finance.routingNumber(),
-				accountNum: faker.finance.account(),
-				status: "Active",
-			},
-			dateRange: "2020-2021"
-		};
-
-		masterListJSON.jsonList.push(entity);
-	}
-};
-
-axios({
-	method: "get",
-	url: "https://www.international.gc.ca/world-monde/assets/office_docs/international_relations-relations_internationales/sanctions/sema-lmes.xml",
-}).then(function (response) {
-	var data = JSON.parse(xmlParser.toJson(response.data));
-	insertDataIntoMasterList(data);
-	consolidatedList = [...masterListJSON.jsonList];
-});
+consolidatedList = [...ccas_list];
 
 let lteListData = [];
 
