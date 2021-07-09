a7d07719ede67f4e5cb39a86ffd796f4fc0921d9
Amy Zhou <amy.zhou@139gc.onmicrosoft.com>
2021-07-09 14:05:01 -0400

    refactor app.js and unlist

--- app.js
+++ app.js
@@ -124,55 +124,11 @@ axios({
 
 var finalList = [];
 
-axios({
-	method: "get",
-	url: "https://scsanctions.un.org/resources/xml/en/consolidated.xml",
-}).then(function (response) {
-	data = response.data;
 
-	var result = JSON.parse(xmlParser.toJson(data));
 
-	var filteredList = result.CONSOLIDATED_LIST.INDIVIDUALS.INDIVIDUAL;
-	//console.log(filteredList);
-	for (i = 0; i < filteredList.length; i++) {
-		var name = filteredList[i].FIRST_NAME +
-			" " +
-			filteredList[i].SECOND_NAME +
-			" " +
-			filteredList[i].THIRD_NAME;
-		var document = filteredList[i].INDIVIDUAL_DOCUMENT.TYPE_OF_DOCUMENT +
-			" :" +
-			filteredList[i].INDIVIDUAL_DOCUMENT.NUMBER;
-		var dob = filteredList[i].INDIVIDUAL_DATE_OF_BIRTH.DATE;
-		var address = filteredList[i].INDIVIDUAL_ADDRESS.COUNTRY;
-		finalList[i] = {
-			name: name,
-			date: dob,
-			link: "https://scsanctions.un.org/resources/xml/en/consolidated.xml",
-			address: {
-				streetNum: faker.datatype.number(),
-				street: faker.address.streetName(),
-				city: faker.address.cityName(),
-				prov: faker.address.state(),
-				postal: faker.address.zipCodeByState(),
-			},
-			person_id: {
-				idType: "Driver's License",
-				idNumber: faker.finance.routingNumber(),
-				idJuristiction: faker.address.state(),
-			},
-			accountInfo: {
-				locale: "Domestic",
-				institution: "TD",
-				transitNum: faker.finance.routingNumber(),
-				accountNum: faker.finance.account(),
-				status: "Active",
-			},
-			dateRange: "2020-2021"
-		};
-	}
-	consolidatedList = [...finalList];
-});
+var unlist = require("./api/unlist").masterListJSON;
+
+consolidatedList = [...unlist.jsonList];
 
 var filteredData = [];
 
